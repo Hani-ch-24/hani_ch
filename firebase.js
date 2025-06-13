704d1d0a3780af74b01aed0bdd40b89ab8d84d09
Hani-ch-24 <chhanou8@gmail.com>
2025-06-13 16:00:28 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,24 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBXVAmLb-ZS4fF4dEixgTgSxmorBjqbHpA",
+  authDomain: "my-website-hani.firebaseapp.com",
+  databaseURL: "https://my-website-hani-default-rtdb.firebaseio.com",
+  projectId: "my-website-hani",
+  storageBucket: "my-website-hani.firebasestorage.app",
+  messagingSenderId: "1025517016986",
+  appId: "1:1025517016986:web:43eef2cc93edc060b88c23",
+  measurementId: "G-CDHHGD6CP1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const analytics = getAnalytics(app);
 
 export { database };
